9aa9cb9b4e006e212f7884aaeb8dd8e4b50accee
Catherine Addison <caddison4@my.bcit.ca>
2023-04-25 15:09:06 -0700

    updated navigation for Create and Invite List

--- frontend/src/views/hosts/events/CreateEvent.js
+++ frontend/src/views/hosts/events/CreateEvent.js
@@ -1,8 +1,16 @@
 import { View, Text, Button } from "react-native";
+import InviteList from "./InviteList";
 export default function CreateEvent({ navigation }) {
+	const eventId = 1;
 	return (
 		<View>
-			<Text>Create Event</Text>
+			<Button
+				title="Set Invite List"
+				onPress={() =>
+					navigation.navigate("InviteList", {
+						eventId: eventId,
+					})
+				}></Button>
 		</View>
 	);
 }
