a72c029de1fa6762cfc9d0c6afb1702904621a83
yukoBCIT <yishida2@my.bcit.ca>
2023-04-27 13:53:50 -0700

    reverted

--- frontend/App.js
+++ frontend/App.js
@@ -13,46 +13,6 @@ const App = () => {
   return (
     <Provider store={store}>
       <Navigation />
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Events"
-          screenOptions={{
-            headerStyle: {
-              backgroundColor: "#607D8B",
-            },
-            headerTintColor: "#fff",
-            headerTitleStyle: {
-              fontWeight: "bold",
-            },
-          }}
-        >
-          {authenticated == true ? (
-            <Stack.Screen name="AuthForm" component={AuthForm} />
-          ) : (
-            <>
-              <Stack.Screen name="EventsList" component={EventsList} />
-              <Stack.Screen name="EventsCal" component={EventsCal} />
-              <Stack.Screen name="MainProfile" component={MainProfile} />
-              <Stack.Screen
-                name="Events"
-                component={Events}
-                options={{
-                  title: "Event App",
-                  headerRight: () => <ProfileNavButton />,
-                }}
-              />
-              <Stack.Screen name="EventListItem" component={EventListItem} />
-              <Stack.Screen name="EventDetails" component={EventDetails} />
-              <Stack.Screen name="Confirmation" component={Confirmation} />
-              <Stack.Screen name="QRCode" component={QRCode} />
-              <Stack.Screen
-                name="ProfileNavButton"
-                component={ProfileNavButton}
-              />
-            </>
-          )}
-        </Stack.Navigator>
-      </NavigationContainer>
     </Provider>
   );
 };
